e5e75a8a268d057a8fd8d7f0fd59f69d62c100e9
Juan Antonio Ruiz <Mdulo@JUANAN>
2019-03-07 08:50:07 +0100

    Segundo cambio

--- src/repositorioEntorno_b/Principal.java
+++ src/repositorioEntorno_b/Principal.java
@@ -3,7 +3,7 @@ package repositorioEntorno_b;
 public class Principal {
 	
 	public static void main (String[]args) {
-		System.out.println("Vamos a operar");
+		System.out.println("Vamos a operar en remoto");
 	}
 
 }
