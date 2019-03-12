0fea56cde9ebee64e18be790d335be83471f2e74
Alvaro <sesilva.93@gmail.com>
2019-03-12 12:15:16 +0100

    "multiplicar" added

--- src/repositorioEntorno_b/Util.java
+++ src/repositorioEntorno_b/Util.java
@@ -1,5 +1,13 @@
 package repositorioEntorno_b;
 
 public class Util {
+	
+	public void multiplicar(int num1, int num2) {
+		
+		System.out.println("Multiplica");
+		
+		System.out.println(num1*num2);
+
+	}
 
 }
